8a5c03d6de166b5064beb0f9852b831bb89d6cf0
leeduckgo <albertschr@163.com>
2022-03-05 23:51:41 +0800

    feat: update headers

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,10 +5,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/WeLightProject/Tai-Shang-Voxel-Handler" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="TaiShang Voxel Handler"
+        subTitle="handle voxel to html page, NFT and actual Model by 3D printer!"
         style={{ cursor: "pointer" }}
       />
     </a>
